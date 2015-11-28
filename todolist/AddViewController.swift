75729c76df57986d7e8b2953c94ef914780c5727
Loïc Monney <loic.monney@gmail.com>
2015-11-28 20:01:33 +0100

    Disable/Enable save button only if task is not empty

--- todolist/AddViewController.swift
+++ todolist/AddViewController.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 
-class AddViewController: UIViewController {
+class AddViewController: UIViewController, UITextFieldDelegate {
     
+    // Tasks container
     var taskContainer : TaskContainer?
     
     @IBOutlet weak var descriptionField: UITextField!
-    
     @IBOutlet weak var urgentToggle: UISwitch!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        descriptionField.delegate = self
+        saveButton.enabled = false
+    }
+    
+    // Save button pressed
     @IBAction func save(sender: AnyObject) {
         let description : String = descriptionField.text!
         let urgent : Bool = urgentToggle.on
@@ -24,7 +32,20 @@ class AddViewController: UIViewController {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    // Cancel
     @IBAction func cancel(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // Delegate ----------------------------------------------------------------------------------
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+        saveButton.enabled = descriptionField.text?.characters.count != 0
+        return true
     }
 }
